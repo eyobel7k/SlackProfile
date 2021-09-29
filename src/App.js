e84a38b954307ec811ff94cc89a0a6c025746cc7
Eyobel Legese <eyobel7k@gmail.com>
2021-09-28 17:57:34 -0700

    created react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import Image from './Image/Eyob.png'
 import './App.css';
+// import "./img.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <div className="photo">
+        <img alt='Eyobel' src={Image} width="300px" heigh="300px" />
+        
+     
+
+      </div>
+      <div className='Wrapp'>
+        <h3>Eyobel Legese he/him {' '}<span className="logged-in">●</span></h3>
+        
+        <h5>React Native Apprentice</h5>
+
+
+        </div>
+      
+      </div>
+      
+        
+    
   );
 }
 
